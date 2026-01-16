413bbe10cb7e36c9f630b71d4ff5fd11f1d0af7b
Ahsan24688 <muhammadahsan9122@gmail.com>
2026-01-17 01:52:17 +0500

    Project Completed

--- JS/Assignment 1/Calculator.js
+++ JS/Assignment 1/Calculator.js
@@ -1,28 +1,51 @@
 var input = document.getElementById("input");
 var output = document.getElementById("output");
+var myhistory = [];
 
-function Enterdata(entrdata){
-    
-    if(entrdata == "clear"){
+function Enterdata(entrdata) {
+
+    if (entrdata == "clear") {
         input.innerText = "";
         output.innerText = "0";
         return
     }
 
-    if(entrdata == "backspace"){
-        input.innerText = input.innerText.slice(0,-1);
+    if (entrdata == "backspace") {
+        input.innerText = input.innerText.slice(0, -1);
         return
     }
-    
+
     input.innerText += entrdata;
 }
-function calculator(){
+function calculator() {
     var result = eval(input.innerText);
 
-    if(Number.isInteger(result)){
+    if (Number.isInteger(result)) {
         output.innerText = result;
     } else {
         output.innerText = result.toFixed(2);
     }
+    myhistory.push(input.innerText + " = " + output.innerText);
 }
 
+function showtime() {
+    var time = new Date();
+    var presenttime = document.getElementById("time");
+    var newtime = presenttime.innerText = time.toLocaleTimeString();
+    return newtime;
+}
+showtime();
+setInterval(showtime, 1000);
+
+function toseemyhistory() {
+    var historyDiv = document.getElementById("historyDiv");
+    historyDiv.innerHTML = '';
+
+    if (myhistory.length === 0) {
+        historyDiv.innerText = "No History Available";
+    } else {
+        for (var i = 0; i < myhistory.length; i++) {
+            historyDiv.innerHTML += myhistory[i] + "<br>";
+        }
+    }
+}
