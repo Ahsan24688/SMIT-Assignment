e90d1ae8aeb807166e04491da06903190ee70e9d
Ahsan24688 <muhammadahsan9122@gmail.com>
2026-01-29 14:28:40 +0500

    Project Completed

---
+++ JS/Assignment 2/README.md
@@ -0,0 +1,2 @@
+# Calculator
+[Page_Link](file:///D:/SMIT%20Course/SMIT-Assignment/JS/Assignment%202/index.html)
--- JS/Assignment 2/clock.js
+++ JS/Assignment 2/clock.js
@@ -1,70 +1,72 @@
 var hourdisplay = document.getElementById("gethour");
 var minutedisplay = document.getElementById("getminute");
 var seconddisplay = document.getElementById("getsecnd");
-var displayTP = document.getElementById("timeperiod");
-var timeperiod = "";
-var greetingtext = document.getElementById("greet");
-var greet = "";
+var displayTPEl = document.getElementById("timeperiod");
+var greetingEl = document.getElementById("greet");
+
 function digitalClock(){
     var time = new Date();
     var hour = time.getHours();
     var min = time.getMinutes();
     var sec = time.getSeconds();
 
+    var period = "AM";
+    var hour12 = hour;
 
-    if(hour === 0 ){
-        hour = 12;
-        timeperiod = "AM";
-    }
-    else if(hour >= 1 && hour <= 11){
-        timeperiod = "AM";
-    }
-    else if(hour == 12){
-        timeperiod = "PM";
-    }
-    else{
-        hour -= 12;
-        timeperiod = "PM";
+    if(hour === 0){
+        hour12 = 12;
+        period = "AM";
+    } else if(hour === 12){
+        hour12 = 12;
+        period = "PM";
+    } else if(hour > 12){
+        hour12 = hour - 12;
+        period = "PM";
+    } else {
+        period = "AM";
     }
 
 
-    if(hour < 10){
-        hour = "0" + hour;
+    var hourStr;
+    if(hour12 < 10){
+        hourStr = "0" + String(hour12);
+    } else {
+        hourStr = String(hour12);
     }
-    else{
-        hour = hour;
-    }
+
+    var minStr;
     if(min < 10){
-        min = "0" + min;
+        minStr = "0" + String(min);
+    } else {
+        minStr = String(min);
     }
-    else{
-        min = min;
-    }
+
+    var secStr;
     if(sec < 10){
-        sec = "0" + sec;
+        secStr = "0" + String(sec);
+    } else {
+        secStr = String(sec);
     }
-    else{
-        sec = sec;
+    
+
+    var greetText = "";
+    if(period === "AM"){
+        greetText = "Good Morning! 🌞";
+    } else { // PM
+        if(hour12 >= 1 && hour12 <= 4){
+            greetText = "Good Afternoon! ☀️";
+        } else if(hour12 >= 5 && hour12 <= 8){
+            greetText = "Good Evening! 🌇";
+        } else {
+            greetText = "Good Night! 🌙";
+        }
     }
 
-    if(timeperiod === "AM"){
-        greet = "Good Morning! 🌞";
-    }
-    if(timeperiod === "PM" && hour >= 1 && hour <= 4){
-        greet = "Good Afternoon! ☀️";
-    }
-    if(timeperiod === "PM" && hour >= 5 && hour <= 8){
-        greet = "Good Evening! 🌇";
-    }
-    if(timeperiod === "PM" && hour >= 9 && hour <= 11){
-        greet = "Good Night! 🌙";
-    }    
-
-    hourdisplay.innerText = hour;
-    minutedisplay.innerText = min;
-    seconddisplay.innerText = sec;
-    displayTP.innerText = timeperiod;
-    greetingtext.innerText = greet;
+    hourdisplay.innerText = hourStr;
+    minutedisplay.innerText = minStr;
+    seconddisplay.innerText = secStr;
+    displayTPEl.innerText = period;
+    if(greetingEl) greetingEl.innerText = greetText;
 }
 digitalClock();
 setInterval(digitalClock, 1000);
