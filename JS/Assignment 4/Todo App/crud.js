e74f306d4500e7b678873bffe4eac5b2897a9656
Ahsan24688 <muhammadahsan9122@gmail.com>
2026-02-21 13:06:40 +0500

    Todo Pending

--- JS/Assignment 4/Todo App/crud.js
+++ JS/Assignment 4/Todo App/crud.js
@@ -3,34 +3,44 @@ var Usersdata = [];
 
 function add() {
 
-    var getid = document.getElementById("id").value;
+    // var getid = document.getElementById("id").value;
+   
     var getname = document.getElementById("name").value;
     var getage = document.getElementById("age").value;
     var getcity = document.getElementById("city").value;
-    var contact = document.getElementById("contact").value;
+    // var contact = document.getElementById("contact").value;
+
+     var userinput ={
+        id: (new Date().getTime() + Math.floor(Math.random()*100)),
+        createddate: (new Date().getDate() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getFullYear()),
+        username: getname,
+        userage: getage,
+        usercity: getcity,
+    }
 
     var pushdata = document.getElementById("table_body");
-    if (getid == "" && getname == "" && getage == "" && getcity == "" && contact == "") {
+    if (getname == "" && getage == "" && getcity == "") {
         alert("Please Enter a Data");
         return;
     }
-    Usersdata.push(getid,getname, getage, getcity, contact);
-    // console.log(Usersdata);
+    Usersdata.push(userinput);
     tablebody = `<tr>
-                <td> ${getid} </td>
-                <td> ${getname} </td>
-                <td> ${getage} </td>
-                <td> ${getcity} </td>
-                <td> ${contact} </td>
+                <td> ${userinput.id} </td>
+                <td> ${userinput.username} </td>
+                <td> ${userinput.userage} </td>
+                <td> 
+                     <button onClick="edituser(${userinput.id})" class="action_btn"> Edit </button>
+                     <button onClick="deleteuser(${userinput.id})" class="action_btn"> Delete </button>
+                </td>
+                <td> ${userinput.createddate} </td>
                 </tr>`;
-    // tablebody.tr.td.style.padding = "10px";            
     pushdata.innerHTML += tablebody;
-    if(getid == getid && getname == getname && getage == getage && getcity == getcity && contact == contact){
-        document.getElementById("id").value = "";
+    if(getname == getname && getage == getage && getcity == getcity){
+        // document.getElementById("id").value = "";
         document.getElementById("name").value = "";
         document.getElementById("age").value = "";
         document.getElementById("city").value = "";
-        document.getElementById("contact").value = "";
+        // document.getElementById("contact").value = "";
         return;
     }
 }
