0024c03dc9e590d23799ad30c9ae2064ab75b7a9
Ahsan24688 <muhammadahsan9122@gmail.com>
2026-05-07 17:19:53 +0500

    Firebase Completed

--- JS/Assignment 5/firebaseconfig.js
+++ JS/Assignment 5/firebaseconfig.js
@@ -9,13 +9,7 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyBK2ZE-E32WTKoPg-C3XSKQQkTSUEgdeVY",
-    authDomain: "todo-app-f415e.firebaseapp.com",
-    projectId: "todo-app-f415e",
-    storageBucket: "todo-app-f415e.firebasestorage.app",
-    messagingSenderId: "75038652418",
-    appId: "1:75038652418:web:ea666b14af3ccd6840cf1b",
-    measurementId: "G-901Q74G5EN"
+   
   };
 
   // Initialize Firebase
